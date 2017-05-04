a7d4cee6cdce598629bfef6d30989318a797264b
Manobhav Jain <manobhav.jain@gmail.com>
2017-05-04 17:18:42 +0530

    final project

---
+++ Demonet_with_UI/Model/Demon.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from datetime import datetime,timedelta
+from tkinter import messagebox
+
+
+class Demon:
+    def __init__(self):
+        os.chdir('/home/manobhav/PycharmProjects/demonetisation analysis')
+        self.ndata=pd.read_csv('Data/demonetlatlong.csv')
+        self.ndata.Date = pd.to_datetime(self.ndata.Date).dt.normalize()
+        self.ndata.sort_values(by='Date',inplace=True)
+        self.ndata['year'] = self.ndata.Date.dt.year
+        self.ndata['month'] = self.ndata.Date.dt.month
+        self.ndata['day'] = self.ndata.Date.dt.day
+
+
+
+    def getBarSentiment(self,param):
+        temp = pd.crosstab(self.ndata[param], self.ndata['Sentiment']).apply(lambda r: r / r.sum(), axis=1)
+        temp.plot(kind='bar', stacked=True)
+        k = pd.DataFrame(temp.idxmax(axis=0))
+        string="Max positive sentiment is in "+str(k.loc[1][0])+".\n Max negative sentiment is in "+str(k.loc[-1][0])
+        messagebox._show(message=string)
+        plt.show()
+
+    def showmap(self,sentiment):
+        mydata=self.ndata.loc[(self.ndata.Sentiment==sentiment)]
+        col='red'
+        if sentiment==1:
+            col='green'
+        from mpl_toolkits.basemap import Basemap
+        m = Basemap(projection='mill', llcrnrlat=6.74678, urcrnrlat=35.674520,
+                    llcrnrlon=68.03215, urcrnrlon=97.40238, resolution='c', epsg=4269)
+        x, y = m(tuple(mydata.Long[(mydata.Long.isnull() == False)]), tuple(mydata.Lat[(mydata.Lat.isnull() == False)]))
+        plt.figure(figsize=(40, 20))
+        m.arcgisimage(service="NatGeo_World_Map", xpixels=3000,verbose=True)
+        m.plot(x, y, 'ro', markersize=15, alpha=0.3,color=col)
+        plt.show()
+
+    def showDateGraph(self,startdate,enddate,sentiment,paper,granularity):
+        d1 = datetime.strptime(startdate, "%Y-%m-%d")-timedelta(days=1)
+        d2 = datetime.strptime(enddate, "%Y-%m-%d")+timedelta(days=1)
+        print(startdate,enddate,sentiment,paper,granularity)
+        alldata=self.ndata[(self.ndata.Sentiment.isin(sentiment))&(self.ndata.Paper.isin(paper))&(self.ndata.Date > d1) & (self.ndata.Date < d2)]
+        print(alldata)
+
+
+        if sentiment==[-1,1]:
+            Group = alldata[alldata.Sentiment==-1].groupby(granularity)['day'].count() / alldata.groupby(granularity)['day'].count()
+        else:
+            Group = alldata.groupby(granularity)['day'].count()
+
+        Group.plot(kind='line')
+        plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Demonet_with_UI/View/input_file.py
@@ -0,0 +1,113 @@
+from tkinter import *
+from tkinter import messagebox
+from Model import Demon
+
+def barplot():
+    demon.getBarSentiment(var.get())
+
+def getMap():
+    demon.showmap(var2.get())
+    master.quit()
+
+def getDateGraph():
+    sentiment=[]
+    granularity=[]
+    paper=[]
+
+
+
+    if sent.get()==0:
+        sentiment.extend([-1,1])
+    else:
+        sentiment.append(sent.get())
+
+    if gran.get()=='day':
+        granularity.extend(['year','month','day'])
+    if gran.get()=='month':
+        granularity.extend(['year','month'])
+    if gran.get()=='year':
+        granularity.append('year')
+
+    if pap.get()=="All":
+        paper.extend(['toi','The Hindu'])
+    else:
+        paper.append(pap.get())
+
+    demon.showDateGraph(startDateEntry.get(),endDateEntry.get(),sentiment=sentiment,granularity=granularity,paper=paper)
+
+master = Tk()
+demon=Demon.Demon()
+
+
+labelText3=StringVar()
+labelText3.set("Get Sentiment bargraph based in the following attributes")
+labelDir3=Label(master, textvariable=labelText3)
+labelDir3.pack()
+var=StringVar(master)
+var.set("Paper")
+
+option=OptionMenu(master,var,*['Category','City','Paper'])
+option.pack()
+
+button=Button(master,text="plot",command=barplot)
+button.pack()
+
+
+
+
+labelText4=StringVar()
+labelText4.set("Get Map based on Sentiment")
+labelDir4=Label(master, textvariable=labelText4)
+labelDir4.pack()
+
+var2=IntVar(master)
+var2.set(-1)
+option2=OptionMenu(master,var2,*[-1,1])
+option2.pack()
+
+mapbutton=Button(master,text='GetMap',command=getMap)
+mapbutton.pack()
+
+
+labelText5=StringVar()
+labelText5.set("Get Sentiment Timeline")
+labelDir5=Label(master, textvariable=labelText5)
+labelDir5.pack()
+
+labelText1=StringVar()
+labelText1.set("Enter Start Date Format- YYYY-MM-DD")
+labelDir1=Label(master, textvariable=labelText1)
+labelDir1.pack()
+startDateEntry=Entry(master)
+startDateEntry.pack()
+
+
+labelText2=StringVar()
+labelText2.set("Enter End Date Format- YYYY-MM-DD")
+labelDir2=Label(master, textvariable=labelText2)
+labelDir2.pack()
+endDateEntry=Entry(master)
+endDateEntry.pack()
+
+sent=IntVar(master)
+sent.set(-1)
+option3=OptionMenu(master,sent,-1,1,0)
+option3.pack()
+
+gran=StringVar(master)
+gran.set('year')
+option4=OptionMenu(master,gran,'year','month','day')
+option4.pack()
+
+pap=StringVar(master)
+pap.set('toi')
+option5=OptionMenu(master,pap,'toi','The Hindu','All')
+option5.pack()
+
+Datebutton=Button(master,text='Dategraph',command=getDateGraph)
+Datebutton.pack()
+
+mainloop()
+
+
+
